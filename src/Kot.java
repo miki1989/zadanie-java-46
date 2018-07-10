ccb0dd81519313d314dee5a6c159117de67d6571
miki1989 <mikolajheller@gmail.com>
2018-07-10 14:01:03 +0200

    Change from ASCII(int) to char and printed KOT

--- src/Kot.java
+++ src/Kot.java
@@ -2,12 +2,25 @@ import java.util.Scanner;
 
 public class Kot {
     public static void main(String[] args) {
-        Scanner scan1 = new Scanner(System.in);
+
+        /*Scanner scan1 = new Scanner(System.in);
         String name;
 
         name = scan1.nextLine(); //zakładam, że użytkownik wpisze po prostu "KOT" i tym samym uzyskamy pożądaną odpowiedź;
         System.out.println(name);
 
         scan1.close();
+        */
+        //wartosci dla znaków przyjąłem z kodu ASCII i przez rzutowanie zamieniłem z int na char i połączyłem.
+        int ascii = 75;
+        int ascii2 = 79;
+        int ascii3 = 84;
+        char znak = (char)ascii;
+        char znak2 = (char)ascii2;
+        char znak3 = (char)ascii3;
+        System.out.print(znak);
+        System.out.print(znak2);
+        System.out.print(znak3);
+
     }
 }
